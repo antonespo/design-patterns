1b026c4d5b99657ed17e530fcbbefaef274d282e
AntonioE <antonio.esposito4@live.it>
2020-12-22 17:26:57 +0100

    Basic implementation without OCP

--- SOLID-principles/Program.cs
+++ SOLID-principles/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SOLID_principles
 {
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.Size == size)
+                    yield return p; 
+            }
+        }
+
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+            {
+                if (p.Color == color)
+                    yield return p;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            Console.WriteLine("Green products: ");
+            foreach(var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green");
+            }
         }
     }
 }
