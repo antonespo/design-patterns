bdbd154a66e7d59ff51102f59d4963e5c604e34c
AntonioE <antonio.esposito4@live.it>
2021-01-07 18:54:30 +0100

    Fluent Builder

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BuilderPattern
+{
+    public class Person
+    {
+        public string Name;
+        public string Position;
+    }
+
+    public class PersonInfoBuilder
+    {
+        protected Person person = new Person(); 
+
+        public PersonInfoBuilder Called(string name)
+        {
+            person.Name = name;
+            return this; 
+        }
+
+        public PersonInfoBuilder WorkAs(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new PersonInfoBuilder();
+            builder.Called("Antonio").WorkAs("Software Developer"); 
+            
+        }
+    }
+}
