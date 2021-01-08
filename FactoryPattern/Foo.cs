d8b8b99da725ba0c2056e41bb76f215569205bd4
AntonioE <antonio.esposito4@live.it>
2021-01-08 23:03:58 +0100

    Factory for specic objects

---
+++ FactoryPattern/Foo.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Foo
+    {
+        private readonly string name;
+
+        private Foo(string name)
+        {
+            this.name = name;
+        }
+
+        private async Task InitAsync()
+        {
+            await Task.Delay(1000);
+        }
+
+        public static async Task<Foo> CreateAsync(string name)
+        {
+            var result = new Foo(name);
+            await result.InitAsync();
+            return result;
+        }
+    }
+}
---
+++ FactoryPattern/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+
+
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Point Origin => new Point(0, 0); // Property
+        public static Point Origin2 = new Point(0, 0); // Field (better because we initialize once) 
+
+
+        public static class Factory
+        {
+            // Factory method
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+}
