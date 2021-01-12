1c0b2d93dbba72e58c7dd1d1bfcbb596a0e18ba1
AntonioE <antonio.esposito4@live.it>
2021-01-12 23:56:42 +0100

    Singleton project

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingletonPattern
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string male);
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+
+        public SingletonDatabase()
+        {
+            Console.WriteLine("Initializing database");
+            //capitals = File.ReadAllLines("capitals.txt")
+            //    .Batch(2);
+            //.ToDictionary(
+            //list => list.ElementAt(0).Trim(),
+            //list => int.Parse(list.ElementAt(1)));
+        }
+
+        public int GetPopulation(string male)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
