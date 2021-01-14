796b29e791253bbe42a2d4cce6127355965a7f9c
AntonioE <antonio.esposito4@live.it>
2021-01-14 22:49:30 +0100

    Singleton without DI and with DI

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -1,30 +1,50 @@
-﻿using System;
+﻿using Autofac;
+using MoreLinq;
+using SingletonPattern.WithoutDI;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace SingletonPattern
 {
-    public interface IDatabase
+    public class ConfigurableRecordFinder
     {
-        int GetPopulation(string male);
+        private IDatabase database;
+
+        public ConfigurableRecordFinder(IDatabase database)
+        {
+            this.database = database;
+        }
+
+        public int GetTotalPopulation(IEnumerable<string> names)
+        {
+            int result = 0;
+            foreach (var name in names)
+            {
+                result += database.GetPopulation(name);
+            }
+            return result;
+        }
     }
 
-    public class SingletonDatabase : IDatabase
+    public class OrdinaryDatabase : IDatabase
     {
         private Dictionary<string, int> capitals;
 
-        public SingletonDatabase()
+        public OrdinaryDatabase()
         {
             Console.WriteLine("Initializing database");
-            //capitals = File.ReadAllLines("capitals.txt")
-            //    .Batch(2);
-            //.ToDictionary(
-            //list => list.ElementAt(0).Trim(),
-            //list => int.Parse(list.ElementAt(1)));
+            capitals = File.ReadAllLines("capitals.txt")
+            .Batch(2)
+            .ToDictionary(
+            list => list.ElementAt(0).Trim(),
+            list => int.Parse(list.ElementAt(1)));
         }
 
-        public int GetPopulation(string male)
+        public int GetPopulation(string name)
         {
-            throw new NotImplementedException();
+            return capitals[name];
         }
     }
 
@@ -32,6 +52,21 @@ namespace SingletonPattern
     {
         private static void Main(string[] args)
         {
+            // Singleton w/ DI
+            var cb = new ContainerBuilder();
+            cb.RegisterType<OrdinaryDatabase>().As<IDatabase>().SingleInstance();
+            cb.RegisterType<ConfigurableRecordFinder>();
+            using (var c = cb.Build())
+            {
+                var rf = c.Resolve<ConfigurableRecordFinder>();
+                var tot = rf.GetTotalPopulation(new[] { "Tokyo", "New York" });
+                Console.WriteLine(tot);
+            }
+
+            // Singleton w/o DI
+            //var db = SingletonDatabase.Instance;
+            //var city = "Tokyo";
+            //Console.WriteLine($"{city} has popilation {db.GetPopulation(city)}");
         }
     }
 }
---
+++ SingletonPattern/SingletonWithoutDI.cs
@@ -0,0 +1,38 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SingletonPattern.WithoutDI
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Initializing database");
+            capitals = File.ReadAllLines("capitals.txt")
+            .Batch(2)
+            .ToDictionary(
+            list => list.ElementAt(0).Trim(),
+            list => int.Parse(list.ElementAt(1)));
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+
+        private static Lazy<SingletonDatabase> instance =
+            new Lazy<SingletonDatabase>(() => new SingletonDatabase());
+
+        public static SingletonDatabase Instance => instance.Value;
+    }
+}
