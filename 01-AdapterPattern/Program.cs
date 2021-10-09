094d71e615feb1116367447dc8022045c0c5cb10
AntonioE <antonio.esposito4@live.it>
2021-10-09 14:13:23 +0200

    Implement adapter pattern

---
+++ 01-AdapterPattern/Program.cs
@@ -0,0 +1,103 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace _01_AdapterPattern
+{
+    public class Point
+    {
+        public int X, Y;
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+
+    public class VectorObject : Collection<Line>
+    {
+
+    }
+
+    public class VectorRectangle : VectorObject
+    {
+        public VectorRectangle(int x, int y, int width, int height)
+        {
+            Add(new Line(new Point(x, y), new Point(x + width, y)));
+            Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+            Add(new Line(new Point(x, y), new Point(x + width, y + height)));
+            Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+        }
+    }
+
+    public class LineToPointAdapter : Collection<Point>
+    {
+        private static int count;
+
+        public LineToPointAdapter(Line line)
+        {
+            Console.WriteLine($"{++count}: Generating points for line [{line.Start.X},{line.Start.Y}]-[{line.End.X},{line.End.Y}]");
+
+            int left = Math.Min(line.Start.X, line.End.X);
+            int right = Math.Max(line.Start.X, line.End.X);
+            int top = Math.Min(line.Start.Y, line.End.Y);
+            int bottom = Math.Max(line.Start.Y, line.End.Y);
+            int dx = right - left;
+            int dy = line.End.Y - line.Start.Y;
+
+            if (dx == 0)
+            {
+                for (int y = 0; y <= bottom; y++)
+                {
+                    Add(new Point(left, y));
+                }
+            }
+            else if (dy == 0)
+            {
+                for (int x = 0; x <= right; x++)
+                {
+                    Add(new Point(x, top));
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        private static readonly List<VectorObject> vectorObjects =
+            new List<VectorObject>
+            {
+                new VectorRectangle(1, 1, 10, 10),
+                new VectorRectangle(3, 3, 6, 6)
+            };
+
+        public static void DrawPoint(Point p)
+        {
+            Console.Write(".");
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var vectorObject in vectorObjects)
+            {
+                foreach (var line in vectorObject)
+                {
+                    var adapter = new LineToPointAdapter(line);
+                    adapter.ForEach(DrawPoint);
+                }
+            }
+        }
+    }
+}
